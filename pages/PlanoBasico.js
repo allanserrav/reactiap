493ebaf6ab8d073898ebd8eede8ad75dcbbff1e1
Allan Serra <69477615+allanmidia3@users.noreply.github.com>
2022-06-20 08:14:57 -0300

    fluxo para realizar o subscription na tela do plano

--- pages/PlanoBasico.js
+++ pages/PlanoBasico.js
@@ -1,10 +1,75 @@
 import * as React from 'react';
-import { Button, SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import { useEffect } from 'react';
+import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
 import Section from './components/Section';
 import * as RNIap from 'react-native-iap';
 
 const PlanoBasicoScreen = ({ navigation }) => {
     
+    const sku = 'br.com.goldies.app.basico';
+    const productIds = Platform.select({
+        ios: [
+          sku
+        ],
+        android: [
+          sku
+        ]
+      });
+
+    useEffect(() => {
+        initConnection();
+
+        return () => {
+            RNIap.endConnection();
+            console.log('end connection done');
+        };
+    });
+
+    const initConnection = () => {
+        RNIap.initConnection()
+            .then(result => {
+                console.log('connection is => ', result);
+                if (result) {
+                    getPurchases();
+                    getSubscriptions();
+                }
+            })
+            .catch(err => {
+                console.log('error in init => ', err);
+            });
+    }
+
+    const getPurchases = () => {
+        RNIap.getAvailablePurchases()
+            .then(purchases => {
+                console.log('Purchases', purchases);
+            })
+            .catch(err => {
+                console.log('error in getPurchases => ', err);
+            });
+    }
+
+    const getSubscriptions = () => {
+        console.log('Product IDs', productIds);
+        RNIap.getSubscriptions(productIds)
+            .then(subs => {
+                console.log('Subscriptions', subs);
+            })
+            .catch(err => {
+                console.log('error in getSubscriptions => ', err);
+            });
+    }
+
+    const handleOnAssinar = async () => {
+        try {
+            console.log('Realizando o subscription', sku);
+            var result = await RNIap.requestSubscription(sku);
+            console.log('Resposta o subscription', result);
+        } catch (err) {
+            console.warn(err.code, err.message);
+        }
+    };
+
     return (
         <SafeAreaView>
             <StatusBar />
@@ -14,14 +79,13 @@ const PlanoBasicoScreen = ({ navigation }) => {
                 </Section>
                 <Button
                     title="Assinar"
-                    onPress={() =>
-                        navigation.navigate('basico')
-                    }
+                    onPress={handleOnAssinar}
                 />
             </View>
         </SafeAreaView>
     );
   };
+
   const styles = StyleSheet.create({
     view: {
         marginTop: "50%",
